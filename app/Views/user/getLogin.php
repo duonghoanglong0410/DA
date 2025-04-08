8005229f8411a97ac07eb9fed97fdbabcde0865b
hacrot3000 <hacrot3000@gmail.com>
2025-04-08 21:46:44 +0700

    Improve login form

--- app/Views/user/getLogin.php
+++ app/Views/user/getLogin.php
@@ -1,20 +1,19 @@
-<body>
-  <div class="container">
-    <div class="row justify-content-center">
-      <div class="col-md-6">
-        <h2 class="mt-5 text-center"><i class="fas fa-user-circle"></i> Đăng nhập</h2>
-        {error}
-        <div class="alert alert-danger">
-          {mess}
-        </div>
-        {/error}
-
-        <form action="{site_url}user/authencation" method="post" id="loginForm">
-          <div class="form-group">
-            <label for="username"><i class="fas fa-user"></i> Tên đăng nhập</label>
-            <input type="text" name="username" id="username" class="form-control" required autofocus>
+<div class="container mt-3">
+  <div class="row justify-content-center">
+    <div class="col-12 col-md-8 col-lg-6">
+      <div class="form-box">
+        <h2 class="text-center"><i class="fas fa-user-circle"></i> Đăng nhập</h2>
+        <form action="{site_url}user/authencation" method="post" class="getDetail">
+          {error}
+          <div class="alert alert-danger">
+            {mess}
           </div>
-          <div class="form-group">
+          {/error}
+          <div class="mb-3">
+            <label for="username"><i class="fas fa-user"></i> Tên đăng nhập</label>
+            <input type="text" name="username" id="username" class="form-control" value="" required>
+          </div>
+          <div class="mb-3">
             <label for="password"><i class="fas fa-lock"></i> Mật khẩu</label>
             <input type="password" name="password" id="password" class="form-control" required>
           </div>
@@ -27,3 +26,4 @@
       </div>
     </div>
   </div>
+</div>
